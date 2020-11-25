9dc2baa9bd5ebeb13d8f7c1eb2563eceb2fed903
unknown <gabrielyan3g@gmail.com>
2020-11-25 17:54:25 +0400

    more styles and state.map

--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -3,13 +3,38 @@ import './ToDo.css'
 
 
 class ToDo extends Component {
+    state = {
+        tasks: ['fisrt task', 'second', 'third task']
+    }
+
     render() {
         return (
             <div className='ToDo__wrapper'>
-                <input type="text" placeholder='Create new task' />
-                <button>Add</button>
+                <div className='container'>
+                    <div className="input__wrapper">
+                        <div className='input__and__button'>
+                            <input type="text" className='task__input' placeholder='Create new task' />
+                            <button>Add</button>
+                        </div>
+                        <div className='tasks__wrapper'>
+
+                            {this.state.tasks.map((task, index) => {
+                                return (
+                                    <div className='task'>
+                                        <p>{task}</p>
+                                    </div>
+                                )
+                            })}
+
+                        </div>
+                    </div>
+                </div>
             </div>
         )
+
+
+
+
     }
 
 }
