72692a0f745fbf786549b2f9cd777261e1736497
unknown <gabrielyan3g@gmail.com>
2020-12-03 18:47:30 +0400

    fontawesome instaled and some functions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import ToDo from './components/ToDo/ToDo';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
     <div className="main-wrapper">
--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Card, Button, Col, Row, InputGroup, FormControl } from 'react-bootstrap';
+import { Card, Button, Col, Row, InputGroup, FormControl, Container } from 'react-bootstrap';
 import './ToDo.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolderPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 
 class ToDo extends Component {
@@ -18,79 +19,83 @@ class ToDo extends Component {
         })
     }
 
+
     addTask = () => {
         const inputValue = this.state.inputValue
-        const newTasks = [...this.state.tasks]
+        const task = [inputValue, ...this.state.tasks]
+        if (inputValue) {
+            this.setState({
+                tasks: task,
+                inputValue: '',
+            })
+        }
 
-        newTasks.push(inputValue)
+    }
 
-        this.setState({
-            tasks: newTasks,
-            inputValue: '',
-        })
+
+    onKeyPressed = (event) => {
+        if (event.keyCode == 13) {
+            this.addTask()
+        }
     }
 
 
     render() {
-        const { inputValue, tasks } = this.state;
 
+        const { inputValue, tasks } = this.state;
+        const task = tasks.map((task, index) => {
+            return (
+
+                <Col xl={2} md={3} sm={6} xs={12} className='mt-3' key={index}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title className='Card__Title'> {index + 1}. {`${task.slice(0, 8)}...`}</Card.Title>
+                            <Card.Text>
+                                {task}
+                            </Card.Text>
+                            <Button variant="danger" >
+                                <FontAwesomeIcon icon={faTrashAlt} />
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            )
+        })
 
 
 
         return (
-            <div className='ToDo__wrapper'>
-                <div className='container'>
-                    <div className="input__wrapper">
-                        <InputGroup className="mb-3">
-                            <FormControl
-                                placeholder="Create new task"
-                                aria-label="Create new task"
-                                aria-describedby="basic-addon2"
-                                style={{ outline: 'none' }}
-                                value={inputValue}
-                                onChange={this.getInputValue}
-                            />
-                            <InputGroup.Append>
-                                <Button
-                                    style={{ outline: 'none' }}
-                                    variant="outline-secondary"
-                                    onClick={this.addTask}
-                                >Button</Button>
-                            </InputGroup.Append>
-                        </InputGroup>
 
+            <Container className=" mt-3">
+                <InputGroup className="mb-3">
+                    <FormControl
+                        placeholder="Create new task"
+                        aria-label="Create new task"
+                        aria-describedby="basic-addon2"
+                        style={{ outline: 'none' }}
+                        value={inputValue}
+                        onChange={this.getInputValue}
+                        onKeyDown={this.onKeyPressed}
+                    />
+                    <InputGroup.Append>
+                        <Button
+                            disabled={inputValue == ''}
+                            style={{ outline: 'none' }}
+                            variant="outline-secondary"
+                            onClick={this.addTask}
 
-                        <div className='tasks__wrapper'>
-                            <Row>
+                        >
+                            <FontAwesomeIcon icon={faFolderPlus} />
+                        </Button>
+                    </InputGroup.Append>
+                </InputGroup>
 
+                <Row className='justify-content-center'>
+                    {task}
+                </Row>
+            </Container>
 
-                                {tasks.map((task, index) => {
-                                    return (
-
-                                        <Col xl={2} md={3} sm={6} xs={12}>
-                                            <Card>
-                                                <Card.Body>
-                                                    <Card.Title className='Card__Title'> {index + 1} {task}</Card.Title>
-                                                    <Card.Text>
-                                                        {task}
-                                                    </Card.Text>
-                                                    <Button variant="primary">X</Button>
-                                                </Card.Body>
-                                            </Card>
-
-                                        </Col>
-
-                                    )
-                                })}
-                            </Row>
-                        </div>
-                    </div>
-                </div>
-            </div>
         )
-
-
-
 
     }
 
